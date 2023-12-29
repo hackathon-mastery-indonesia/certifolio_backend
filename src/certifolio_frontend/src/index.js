f722ff4ec9a52a42f6a398cf3c47bf0168f24af0
angganion <wahyu.ridho@ui.ac.id>
2023-12-29 22:15:37 +0700

    tes

--- src/certifolio_frontend/src/index.js
+++ src/certifolio_frontend/src/index.js
@@ -1,4 +1,10 @@
-import { certifolio_backend } from "../../declarations/certifolio_backend";
+import { icp_login_backend } from "../../declarations/certifolio_backend";
+import { Actor, HttpAgent } from "@dfinity/agent";
+import {AuthClient} from "@dfinity/auth-client";
+
+import { idlFactory } from "../../declarations/certifolio_backend/certifolio_backend.did.js";
+
+let globalIdentity;
 
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -9,7 +15,7 @@ document.querySelector("form").addEventListener("submit", async (e) => {
   button.setAttribute("disabled", true);
 
   // Interact with foo actor, calling the greet method
-  const greeting = await certifolio_backend.greet(name);
+  const greeting = await icp_login_backend.greet(name);
 
   button.removeAttribute("disabled");
 
@@ -17,3 +23,37 @@ document.querySelector("form").addEventListener("submit", async (e) => {
 
   return false;
 });
+
+//login button
+document.querySelector("#login").addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const authClient = await AuthClient.create();
+  await authClient.login({
+    maxTimeToLive: BigInt(7 * 24 * 60 * 60 * 1000000000),
+    onSuccess: async () => {
+      await handleAuthenticated(authClient);
+    },
+    identityProvider: process.env.DFX_NETWORK === "ic" ? "https://identity.ic0.app/#authorize" : "http://localhost:4943/?canisterId=" + process.env.INTERNET_IDENTITY_CANISTER_ID,
+  });
+  async function  handleAuthenticated(authClient){
+    console.log("Authenticated with identity: ", authClient.getIdentity().getPrincipal().toString());
+    const identity = authClient.getIdentity();
+    globalIdentity = identity;
+    console.log("identity", globalIdentity.getPrincipal().toString());
+    const agent = new HttpAgent({ identity });
+    if (process.env.DFX_NETWORK !== "ic") {
+      agent.fetchRootKey().catch((err) => {
+        console.warn("Unable to fetch root key. Check to ensure that your local replica is running");
+        console.error(err);
+      });
+    }
+    const actor = Actor.createActor(idlFactory, {
+      agent,
+      canisterId: process.env.CANISTER_ID_CERTIFOLIO_BACKEND,
+    });
+    console.log("ppppppp");
+    const principal = await actor.whoami();
+    console.log("principal", principal.toString());
+    };
+}); 
